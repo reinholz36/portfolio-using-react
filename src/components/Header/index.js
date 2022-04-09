fcf39448c7934b4eae5c100a0cb809434274e061
reinholz36 <dreinholz28@gmail.com>
2022-04-09 19:31:11 -0400

    initial styling package

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <header>
+          <div>
+              <Link to="/">
+                  <h1>Reinholz</h1>
+              </Link>
+
+              <nav>
+                  
+              </nav>
+          </div>
+      </header>
+    );
+  }
+  
+  export default Header;
