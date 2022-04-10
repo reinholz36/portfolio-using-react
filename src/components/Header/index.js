d4a905654d90c702848b426b545403338dad680b
reinholz36 <dreinholz28@gmail.com>
2022-04-09 20:08:08 -0400

    updated header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-      <header>
-          <div>
+      <header className="bg-secondary mb-4 py-2 flex-row align-center">
+          <div className="container flex-row justify-space-between-lg justify-center align-center">
               <Link to="/">
                   <h1>Reinholz</h1>
               </Link>
 
-              <nav>
-                  
+              <nav className='text-center'>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/contactme">Contact Me</Link>
+                <Link to="/resume">Resume</Link>
               </nav>
           </div>
       </header>
